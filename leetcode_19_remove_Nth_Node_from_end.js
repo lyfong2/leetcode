feba4f474d08cce576aeb4239fd8982caf67f56d
Danny Lin <40577862+lyfong2@users.noreply.github.com>
2020-05-22 12:10:50 -0400

    Create leetcode_19_remove_Nth_Node_from_end.js

---
+++ leetcode_19_remove_Nth_Node_from_end.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode(0)
+    let slow = dummy;
+    let fast = dummy;
+    dummy.next = head;
+    
+    for(i = 0; i <= n; i++) {
+        fast = fast.next;
+    }
+    while(fast != null){
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    
+    return dummy.next;
+};
+
+
+
+
+
