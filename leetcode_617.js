788c989fdb21480f1bb220373a8c933aa16e58dd
Danny Lin <40577862+lyfong2@users.noreply.github.com>
2020-07-09 12:14:47 -0400

    Create leetcode_617.js

---
+++ leetcode_617.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(t1, t2) {
+    if(t1 === null && t2 === null) return null;
+    if(t1 === null) return t2;
+    if(t2 === null) return t1;
+    
+    let root = new TreeNode(t1.val + t2.val);
+    root.left = mergeTrees(t1.left, t2.left);
+    root.right = mergeTrees(t1.right, t2.right);
+    
+    return root;
+};
+
